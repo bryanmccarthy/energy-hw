6b8582755dc1561a716ca1c40f28f8ab6e82f3b9
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-10-14 13:29:35 +0100

    add events section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 
 function App() {
   return (
-    <div className="border-2 h-screen">
+    <div className="flex flex-col border-2 h-screen">
       {/* Header */}
       <div className="flex justify-between bg-zinc-100 p-2">
         <div>
@@ -47,6 +47,15 @@ function App() {
           <path strokeLinecap="round" strokeLinejoin="round" d="M6 13.5V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 9.75V10.5" />
         </svg>
         <p className="px-2">Filters</p>
+      </div>
+      {/* Events */}
+      <div className="flex-grow overflow-y-auto border-2">
+        <div className="h-20">
+          This is an event
+        </div>
+        <div className="h-20">
+          This is another event
+        </div>
       </div>
     </div>
   );
