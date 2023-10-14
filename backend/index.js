ddf94092a64114443f50f40d0dd846ce34f667fd
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-10-14 16:15:24 +0100

    create routes file

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const Koa = require("koa");
 const parser = require("koa-bodyparser");
 const cors = require("@koa/cors");
+const router = require("./routes/routes");
 const App = new Koa();
 const port = 8000;
 
 App.use(parser())
   .use(cors())
+  .use(router.routes())
   .listen(port, () => {
     console.log(`Server listening http://127.0.0.1:${port}/`);
   });
