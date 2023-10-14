8cdff1f7636fca6c7189d945750dc5a50d3cf668
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-10-14 12:25:19 +0100

    create-react-app with tailwind

---
+++ frontend/README.md
@@ -0,0 +1,5 @@
+# Energy HW Frontend
+
+
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      Energy HW frontend
+    </div>
+  );
+}
+
+export default App;
