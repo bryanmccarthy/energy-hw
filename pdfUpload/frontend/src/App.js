637f15175afdec97192f1094735f77ef5751c739
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-10-15 16:27:25 +0100

    handle pdf download

--- pdfUpload/frontend/src/App.js
+++ pdfUpload/frontend/src/App.js
@@ -10,6 +10,19 @@ function App() {
     setPdfs(data.files);
   }
 
+  const downloadPdf = async (filename) => {
+    const res = await fetch(`http://localhost:8080/download/${filename}`);
+    const blob = await res.blob();
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+
+    a.href = url;
+    a.setAttribute("download", filename);
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+  }
+
   const { isLoading, error, data } = useQuery("pdfs", () => fetchFiles());
 
   return (
@@ -29,9 +42,14 @@ function App() {
       {error && <div>Error: {error.message}</div>}
 
       <div className="flex flex-wrap p-4">
-        <div >
-          {pdfs.map(pdf => (
-            <div key={pdf} className="border-2 h-32 w-28 flex justify-center items-center rounded-lg m-4 bg-gray-200">{pdf}</div>
+        <div>
+          {pdfs?.map((pdf) => (
+            <div className="flex flex-col justify-center items-center m-4">
+              <div key={pdf} className="border-2 h-40 w-40 flex justify-center items-center rounded-lg m-2 bg-gray-200">
+                {pdf}
+              </div>
+              <button onClick={() => downloadPdf(pdf)} className="border-2 p-2 w-40 rounded-lg bg-slate-300">Download</button>
+            </div>
           ))}
         </div>
       </div>
